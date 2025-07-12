7601c07d7033e23a606fda22075936646d6dca9a
viniveloso <viniciusveloso01@gmail.com>
2025-07-12 17:05:40 -0300

    Criação da controller.

---
+++ ContactList/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : Controller
+    {
+        [HttpGet(Name = "GetContact")]
+        public IActionResult Index()
+        {
+            return Ok("erro");
+        }
+
+
+    }
+}
